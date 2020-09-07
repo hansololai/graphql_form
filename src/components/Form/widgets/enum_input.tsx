295ca32f12cebb1832ebb8ee6935c1f980d67740
Han <han.lai321@gmail.com>
2020-09-07 00:58:51 -0400

    use hooks for enum select

--- src/components/Form/widgets/enum_input.tsx
+++ src/components/Form/widgets/enum_input.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import gql from 'graphql-tag';
-import { Query } from '@apollo/react-components';
-import { Select, Spin } from 'antd';
-import { enumTypeQuery } from './__generated__/enumTypeQuery'
+import { Select, notification } from 'antd';
+import { enumTypeQuery, enumTypeQueryVariables } from './__generated__/enumTypeQuery'
+import { useQuery } from '@apollo/react-hooks';
 
 export interface EnumInputProps {
   value: any;
@@ -23,27 +23,24 @@ export const enumQuery = gql`
 
 export const EnumInput: React.FC<EnumInputProps> = (props) => {
   const { value, onChange, enumType } = props;
-  return <Query<enumTypeQuery> query={enumQuery} variables={{ name: enumType }}>
-    {({ data, loading, error }) => {
-      if (loading) return <Spin />;
-      if (!data) {
-        return null;
-      }
-      const { __type } = data;
-      let options: React.ReactElement[] = [];
-      if (__type) {
-        const enumValues = __type.enumValues || [];
-        options = enumValues.map(v => {
-          return <Select.Option key={v.name} value={v.name}>{v.name}</Select.Option>
-        })
-      }
-      return <Select
-        value={value}
-        onChange={onChange}
-      >
-        {options}
-      </Select>
-    }}
-  </Query>
-
+  const {data, loading, error} = useQuery<enumTypeQuery, enumTypeQueryVariables>(enumQuery, {variables:{name: enumType}});
+  if(error){
+    notification.error({
+      message:`Error Fetching options for ${enumType}`,
+      description: error.message,
+    });
+  }
+  const enumValues = React.useMemo(()=>{
+    return data?.__type?.enumValues || [];
+  },[data]);
+  const options = enumValues.map(v => {
+    return <Select.Option key={v.name} value={v.name}>{v.name}</Select.Option>
+  });
+  return <Select
+  value={value}
+  onChange={onChange}
+  loading={loading}
+  >
+    {options}
+  </Select>
 }
